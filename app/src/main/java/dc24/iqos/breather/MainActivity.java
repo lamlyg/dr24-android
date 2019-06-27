53ea7b2e03659483c747619b5c2a1c5be43bc281
lamlyg <lamlyg@naver.com>
2019-06-27 15:09:49 +0900

    메인,상단툴바:수정

--- app/src/main/java/dc24/iqos/breather/MainActivity.java
+++ app/src/main/java/dc24/iqos/breather/MainActivity.java
@@ -1,114 +1,42 @@
 package dc24.iqos.breather;
 
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
-//import android.widget.Toolbar;
-import androidx.appcompat.widget.Toolbar;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.material.navigation.NavigationView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
-
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    ActionBarDrawerToggle drawerToggle;
-    Toolbar toolbar;
+    Handler handler = new Handler();
+    Runnable r = new Runnable() {
+        @Override
+        public void run() {
+            // 4초뒤에 다음화면(MainActivity)으로 넘어가기 Handler 사용
+            Intent intent = new Intent(getApplicationContext(), FeedActivity.class);
+            startActivity(intent); // 다음화면으로 넘어가기
+            finish(); // Activity 화면 제거
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        initLayout();
+        setContentView(R.layout.activity_main);//xml과 java소스 연결
     }
 
     @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.mainfeed:
-                Toast.makeText(this, "mainfeed clicked..", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.mycigarette:
-                Toast.makeText(this, "mycigarette clicked..", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.chart:
-                Toast.makeText(this, "chart clicked..", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.alarm:
-                Toast.makeText(this, "alarm clicked..", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.setting:
-                Toast.makeText(this, "setting clicked..", Toast.LENGTH_SHORT).show();
-                break;
-
-        }
-        drawerLayout.closeDrawer(GravityCompat.START);
-
-        return false;
+    protected void onResume() {
+        super.onResume();
+        // 다시 화면에 들어어왔을 때 예약 걸어주기
+        handler.postDelayed(r, 3000); // 3초 뒤에 Runnable 객체 수행
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
+    protected void onPause() {
+        super.onPause();
+        // 화면을 벗어나면, handler 에 예약해놓은 작업을 취소하자
+        handler.removeCallbacks(r); // 예약 취소
     }
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        drawerToggle.onConfigurationChanged(newConfig);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle your other action bar items...
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void initLayout() {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu_btn);
-
-        drawerLayout = (DrawerLayout) findViewById(R.id.dl_main_drawer_root);
-        navigationView = (NavigationView) findViewById(R.id.nv_main_navigation_root);
-        drawerToggle = new ActionBarDrawerToggle(
-                this,
-                drawerLayout,
-                toolbar,
-                R.string.drawer_open,
-                R.string.drawer_close
-        );
-        drawerLayout.addDrawerListener(drawerToggle);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
 }
-
